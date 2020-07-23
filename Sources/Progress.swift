4aaa556147abb19b74c192eb0d3fd643b729100f
Lane Schwartz <dowobeha@gmail.com>
2020-07-23 13:47:46 -0500

    Work on stderr

--- Sources/Progress.swift
+++ Sources/Progress.swift
@@ -29,6 +29,13 @@
 // MARK: - ProgressBarDisplayer
 import Foundation
 
+extension FileHandle : TextOutputStream {
+  public func write(_ string: String) {
+    guard let data = string.data(using: .utf8) else { return }
+    self.write(data)
+  }
+}
+
 public protocol ProgressBarPrinter {
     mutating func display(_ progressBar: ProgressBar)
 }
